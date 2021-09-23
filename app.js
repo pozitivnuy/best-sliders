61bcbb971832436e9d79ab532dbf7737e5f202c0
pozitivnuy <pozitivnuy@gmail.com>
2021-09-23 21:04:02 +0300

    create keyboard

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const container = document.querySelector('.container');
 const mainSlide = document.querySelector('.main-slide');
 const slidesCount = mainSlide.querySelectorAll('div').length;
 
+
+
 let activeSlideIndex = 0;
 
 sidebar.style.top = `-${(slidesCount -1) *100}vh`;
@@ -16,6 +18,15 @@ upBtn.addEventListener('click', () => {
 downBtn.addEventListener('click', () => {
   chaingSlide('down');
 });
+
+document.addEventListener('keydown', event => {
+  if(event.key === 'ArrowUp'){
+    chaingSlide('up');
+  }else if(event.key === 'ArrowDown'){
+    chaingSlide('down');
+  }
+});
+
 
 function chaingSlide(direction){
   if(direction === 'up'){
